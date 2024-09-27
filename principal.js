1d98c8903653d1681d664ac626cfbdddb28b5d0e
vinnypare <160244724+vinnypare@users.noreply.github.com>
2024-09-27 09:00:20 -0300

    Atualização_v1

--- principal.js
+++ principal.js
@@ -12,6 +12,24 @@ var altura = tdAltura.textContent;
 var tdImc = paciente.querySelector(".info-imc");
 var imcDaLinha = tdImc.textContent;
 
-var imc = peso / (altura * altura);
+var pesoEhValido = true;
+var alturaEhValida = true;
 
-tdImc.textContent = imc;
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+
